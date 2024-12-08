09cebd1a335334fb6a3332f334359964fcfff37b
JValma2024 <jenei.viola@gmail.com>
2024-12-08 20:32:10 +0100

    Frissítés: HTML és PHP fájlok módosítása

--- GTDIOV_1123/feldolgozo.php
+++ GTDIOV_1123/feldolgozo.php
@@ -1,24 +1,33 @@
-<body>
-<?php
-if (isset($_POST)) {
-    echo "<h2>Űrlap adatok</h2>";
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Feldolgozó</title>
+  </head>
+    <body>
+        <?php
+            if (isset($_POST)) {
+                echo "<h2>Űrlap adatok</h2>";
 
-    $nev = $_POST["nev"];
-    $pin = $_POST["pin"];
-    $fav_fruit = $_POST["fav_fruit"];
-    $age = $_POST["age"];
-    $feet_size = $_POST["feet_size"];
-    $confidence = $_POST["confidence"];
+                $nev = $_POST["nev"];
+                $pin = $_POST["pin"];
+                $fav_fruit = $_POST["fav_fruit"];
+                $age = $_POST["age"];
+                $feet_size = $_POST["feet_size"];
+                $confidence = $_POST["confidence"];
 
-    echo "<p><strong>Név:</strong> " . $nev . "</p>";
-    echo "<p><strong>PIN kód:</strong> " . $pin . "</p>";
-    echo "<p><strong>Kedvenc gyümölcs:</strong> " . $fav_fruit . "</p>";
-    echo "<p><strong>Életkor:</strong> " . $age . "</p>";
-    echo "<p><strong>Lábméret:</strong> " . $feet_size . "</p>";
-    echo "<p><strong>Önbizalom:</strong> " . $confidence . "</p>";
-} else {
-    echo "<h2>Űrlap nem lett beküldve!</h2>";
-}
-?>
-<a href="CNY8MP_urlap.html">Vissza az űrlapra.</a>
-</body>
+                echo "<p><strong>Név:</strong> " . $nev . "</p>";
+                echo "<p><strong>PIN kód:</strong> " . $pin . "</p>";
+                echo "<p><strong>Kedvenc gyümölcs:</strong> " . $fav_fruit . "</p>";
+                echo "<p><strong>Életkor:</strong> " . $age . "</p>";
+                echo "<p><strong>Lábméret:</strong> " . $feet_size . "</p>";
+                echo "<p><strong>Önbizalom:</strong> " . $confidence . "</p>";
+            } else {
+                echo "<h2>Űrlap nem lett beküldve!</h2>";
+            }
+        ?>
+
+        <a href="GTDIOV_urlap.html">Vissza az űrlapra.</a>
+    </body>
+</html>
+
